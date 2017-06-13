be1569cb0ba99da127abaad81c2828b4f4412c81
zhourong <317272884@qq.com>
2017-06-13 13:27:34 +0000

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
